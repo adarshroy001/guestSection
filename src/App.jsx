fcf0b520f24023cbcc6582ea6de4fd4a134e9d03
Adarsh Roy <adarshroy2010@gmail.com>
2024-09-26 00:42:06 +0530

    push

---
+++ src/App.jsx
@@ -0,0 +1,114 @@
+import React from 'react';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+
+
+import slide_image_1 from './assets/dummyimage.jpg';
+import slide_image_2 from './assets/dummyimage.jpg';
+import slide_image_3 from './assets/dummyimage.jpg';
+import slide_image_4 from './assets/dummyimage.jpg';
+import slide_image_5 from './assets/dummyimage.jpg';
+import slide_image_6 from './assets/dummyimage.jpg';
+import slide_image_7 from './assets/dummyimage.jpg';
+
+
+
+
+
+function App() {
+
+  return (
+<div className="container">
+
+    <h1 className="heading">
+      Our - Speakers 
+    </h1>
+    <Swiper 
+    effect={'coverflow'}
+    grabCursor={true}
+    centeredSlides={true}
+    loop={true}
+    slidesPerView={'auto'}
+    coverflowEffect={{
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2.5,
+    }}
+    pagination={{ el: '.swiper-pagination', clickable: true }}
+    navigation={{
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+      clickable: true,
+    }}
+    modules={[EffectCoverflow, Pagination, Navigation]}
+    className="swiper_container"
+>
+      <SwiperSlide>
+        <img src={slide_image_1} alt="slide_image" />
+        <h2 className="nameofSpeaker">
+          Lorem ipsum-1
+        </h2>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slide_image_2} alt="slide_image" />
+        <h2 className="nameofSpeaker">
+          Lorem ipsum-2
+        </h2>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slide_image_3} alt="slide_image" />
+        <h2 className="nameofSpeaker">
+          Lorem ipsum-3
+        </h2>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slide_image_4} alt="slide_image" />
+        <h2 className="nameofSpeaker">
+          Lorem ipsum-4
+        </h2>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slide_image_5} alt="slide_image" />
+        <h2 className="nameofSpeaker">
+          Lorem ipsum-5
+        </h2>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slide_image_6} alt="slide_image" />
+        <h2 className="nameofSpeaker">
+          Lorem ipsum-6
+        </h2>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slide_image_7} alt="slide_image" />
+        <h2 className="nameofSpeaker">
+          Lorem ipsum-7
+        </h2>
+      </SwiperSlide>
+
+      <div className="slider-controler">
+          <div className="swiper-button-prev slider-arrow">
+            <ion-icon name="arrow-back-outline"></ion-icon>
+          </div>
+          <div className="swiper-button-next slider-arrow">
+            <ion-icon name="arrow-forward-outline"></ion-icon>
+          </div>
+          <div className="swiper-pagination"></div>
+        </div>
+     
+    </Swiper>
+
+  </div>
+
+  )
+}
+
+export default App
